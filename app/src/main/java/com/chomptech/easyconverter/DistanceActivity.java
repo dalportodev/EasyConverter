2f79eb50512900ed6f82ee69790850dd8e72d426
dr-p <wp7chris@gmail.com>
2017-09-12 17:28:18 -0700

    Iinitial commit re do

---
+++ app/src/main/java/com/chomptech/easyconverter/DistanceActivity.java
@@ -0,0 +1,35 @@
+package com.chomptech.easyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class DistanceActivity extends AppCompatActivity {
+    private EditText in, out;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distance);
+        in = (EditText)findViewById(R.id.editText1);
+        out = (EditText)findViewById(R.id.editText2);
+
+        Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.distance_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner1.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner2.setAdapter(adapter);
+    }
+    public void convert (View view) {
+
+    }
+}
---
+++ app/src/main/java/com/chomptech/easyconverter/MainActivity.java
@@ -0,0 +1,24 @@
+package com.chomptech.easyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+// add methods to change activities
+}
